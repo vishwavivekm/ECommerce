e73091c6d7e14e4352bedda7835d3c64e55447cb
brn.developers <brn.developers@achiever-98.local>
2019-04-26 15:32:22 +0530

    Initial Commit

---
+++ ECommerceApplicationTests/ECommerceApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  ECommerceApplicationTests.swift
+//  ECommerceApplicationTests
+//
+//  Created by brn.developers on 4/26/19.
+//  Copyright © 2019 Vivek. All rights reserved.
+//
+
+import XCTest
+@testable import ECommerceApplication
+
+class ECommerceApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
